14f915c3c6d1ccbfcdfdef3012335f2c72bde6eb
Innocentadika <innocentadika18@gmail.com>
2025-04-01 11:02:03 +0300

    code transfer to sign up and coding button redirect events

--- app/src/main/java/com/example/uniapp/MainActivity.java
+++ app/src/main/java/com/example/uniapp/MainActivity.java
@@ -1,51 +1,31 @@
 package com.example.uniapp;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    EditText fname, user, email, password, repass;
-    Button reg;
-    DBHelper DB;
-    TextView backward, loginr;
+    Button login, register;
 
-
-
-    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-//        login code
-        fname= findViewById(R.id.fname);
-        user= findViewById(R.id.user);
-        email= findViewById(R.id.email);
-        password= findViewById(R.id.password);
-        repass= findViewById(R.id.repass);
-        reg= findViewById(R.id.reg);
-        DB=new DBHelper(this);
-        backward = findViewById(R.id.backward);
-        loginr = findViewById(R.id.loginr);
+    login = findViewById(R.id.login);
+    register = findViewById(R.id.register);
 
-        backward.setOnClickListener( new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Intent intent = new Intent(MainActivity.this, MainActivity.class);
-               startActivity(intent);
-               finish();
-        }
+        register.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
-
-        loginr.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
@@ -53,41 +33,5 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-        reg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String fullname = fname.getText().toString();
-                String name = user.getText().toString();
-                String emai = email.getText().toString();
-                String pass = password.getText().toString();
-                String re = repass.getText().toString();
-
-                if (fullname.equals("") || name.equals("") || emai.equals("") || pass.equals("") || re.equals("")) {
-                    Toast.makeText(MainActivity.this, "Please Fill in all the Fields", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (pass.equals(re)) {
-                        Boolean checkusername = DB.checkusername(name);
-                        if (!checkusername) {
-                            Boolean insert = DB.insertdata(fullname, name, emai, pass);
-                            if (insert) {
-                                Toast.makeText(MainActivity.this, "USER REGISTRATION SUCCESS", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                Toast.makeText(MainActivity.this, "SORRY REGISTRATION FAILED", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(MainActivity.this, "Username already exists", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(MainActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
-
-
     }
 }
